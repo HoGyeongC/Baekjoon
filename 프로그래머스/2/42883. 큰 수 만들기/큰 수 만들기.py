96fb1d86166a2ea8e569785a1105a76a230d3d13
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2023-11-01 01:20:50 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -2,21 +2,20 @@ def solution(number, k):
     answer = ''
     numbers = []
     
-    t = k
     for i in range(len(number)):
         if len(numbers) == 0:
             numbers.append(number[i])
             continue
-        if t == 0:
+        if k == 0:
             for j in range(i, len(number)):
                 numbers.append(number[j])
             break
-        while len(numbers) > 0 and numbers[-1] < number[i] and t > 0:
+        while len(numbers) > 0 and numbers[-1] < number[i] and k > 0:
             numbers.pop()
-            t -= 1
+            k -= 1
         numbers.append(number[i])
     
-    while len(numbers) > len(number) - k:
-        numbers.pop()
+    if k != 0:
+        numbers = numbers[:-k]
     
     return ''.join(numbers)
