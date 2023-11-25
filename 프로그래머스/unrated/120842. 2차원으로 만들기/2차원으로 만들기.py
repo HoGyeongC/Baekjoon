b73b745d50a7364f42aa5a48955472224629204c
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2023-11-25 16:00:46 +0900

    [level unrated] Title: 2차원으로 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120842. 2차원으로 만들기/2차원으로 만들기.py
@@ -0,0 +1,6 @@
+def solution(num_list, n):
+    answer = [[0 for _ in range(n)]for _ in range(len(num_list)//n)]
+    for i in range(len(num_list)):
+        answer[i//n][i%n] = num_list[i]
+        
+    return answer
