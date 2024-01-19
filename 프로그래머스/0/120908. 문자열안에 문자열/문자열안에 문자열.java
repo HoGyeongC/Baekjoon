6455a4ff478a3cae4f9c04bd2aca4731e2bde047
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2024-01-19 14:39:00 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,21 +1,8 @@
 class Solution {
     public int solution(String str1, String str2) {
-        String[] sa1 = str1.split("");
-        String[] sa2 = str2.split("");
-        int lenSa1 = sa1.length, lenSa2 = sa2.length;
-        
-        
-        for(int i=0;i<lenSa1 - lenSa2 + 1;i++){
-            if(sa1[i].equals(sa2[0])){
-                int cnt = 0;
-                for(int j=0;j<lenSa2;j++){
-                    if(sa1[i+j].equals(sa2[j])) cnt++;
-                }
-                if(cnt == lenSa2)
-                    return 1;
-            }
-        }
-        
-        return 2;
+        if(str1.contains(str2))
+            return 1;
+        else
+            return 2;
     }
 }
