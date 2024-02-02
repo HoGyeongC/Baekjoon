dc63664ad71e24fc5a64819e7e28cf8d2f27d89d
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2024-02-02 19:18:26 +0900

    [level 0] Title: 옹알이 (1), Time: 1.13 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        
+        for(int i=0;i<babbling.length;i++){
+            babbling[i] = babbling[i].replaceAll("aya", "!");
+            babbling[i] = babbling[i].replaceAll("ye", "!");
+            babbling[i] = babbling[i].replaceAll("woo", "!");
+            babbling[i] = babbling[i].replaceAll("ma", "!");
+            babbling[i] = babbling[i].replaceAll("!", "");
+            
+            if(babbling[i].equals("")){
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
