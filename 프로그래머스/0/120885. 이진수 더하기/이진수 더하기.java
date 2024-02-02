cdbe662dc9deb31a5ddbfedc449d25239695344c
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2024-02-02 20:43:19 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -2,43 +2,6 @@ import java.util.*;
 
 class Solution {
     public String solution(String bin1, String bin2) {
-        char[] answer = new char[11];
-        char[] bin11 = bin1.toCharArray();
-        char[] bin22 = bin2.toCharArray();
-        int binLen1 = bin11.length, binLen2 = bin22.length;
-        
-        for(int i=0;i<11;i++){
-            answer[i] = '0';
-        }
-        
-        for(int i=10;i>0;i--){
-            if(10-i < binLen1){
-                if(bin11[binLen1 - (10-i) - 1] == '1'){
-                    if(answer[i] == '0'){
-                        answer[i] = '1';
-                    }
-                    else{
-                        answer[i-1] = '1';
-                        answer[i] = '0';
-                    }
-                }
-            }
-            if(10-i < binLen2){
-                if(bin22[binLen2 - (10-i) - 1] == '1'){
-                    if(answer[i] == '0'){
-                        answer[i] = '1';
-                    }
-                    else{
-                        answer[i-1] = '1';
-                        answer[i] = '0';
-                    }
-                }
-            }
-        }
-        
-        
-        Long result = Long.parseLong(String.valueOf(answer));
-        
-        return String.valueOf(result);
+        return Integer.toString(Integer.parseInt(bin1,2) + Integer.parseInt(bin2, 2), 2);
     }
 }
