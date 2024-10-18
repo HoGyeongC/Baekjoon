47e9224582b421c6f4fb03b185e17e2de950b161
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2024-10-18 15:01:23 +0900

    [level 1] Title: 폰켓몬, Time: 5.56 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 1.56 ms
+메모리: 83.8 MB, 시간: 5.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 1일 15:11:55
+2024년 10월 18일 15:01:20
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = nums.length / 2;
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int num : nums){
+            set.add(num);
+        }
+        
+        if(set.size() < answer){
+            answer = set.size();
+        }
+        
+        return answer;
+    }
+}
