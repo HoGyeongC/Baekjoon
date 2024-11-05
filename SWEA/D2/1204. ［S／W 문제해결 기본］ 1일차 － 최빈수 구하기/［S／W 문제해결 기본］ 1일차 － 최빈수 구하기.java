a986a705963482cd0f30593f0c0b6fdc87f84000
최호경 <114250166+HoGyeongC@users.noreply.github.com>
2024-11-05 14:05:22 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 131 ms, Memory: 24,364 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -25,9 +25,8 @@
 //System.out.println(var);		       				   // 문자열 1개 출력하는 예제
 //System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
 /////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
-
+import java.util.*;
+import java.io.*;
 /*
    사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
    이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
@@ -48,33 +47,43 @@ class Solution
 		/*
 		   표준입력 System.in 으로부터 스캐너를 만들어 데이터를 읽어옵니다.
 		 */
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+		int T = Integer.parseInt(br.readLine());
 		/*
 		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 		*/
 
-		for(int test_case = 1; test_case <= T; test_case++){
-			int N = sc.nextInt();
-            int[] student = new int[101];
-            int max = 0, maxidx = 0;
-            int score = 0;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			br.readLine();
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Map<Integer, Integer> score = new HashMap<>();
+            int max = 0;
+            int maxScore = 0;
             
-            for(int i=0;i<1000;i++){
-            	score = sc.nextInt();
-                student[score]++;
-            	if(student[score] > student[maxidx]){
-                	maxidx = score;
-                    max = student[score];
+            while(st.hasMoreTokens()){
+            	int sco = Integer.parseInt(st.nextToken());
+                
+                if(!score.containsKey(sco)){
+                	score.put(sco, 1);
                 }
-                else if(student[score] == student[maxidx] && score > maxidx){
-                	maxidx = score;
+                else{
+                	score.put(sco, score.get(sco)+1);
+                }
+                
+                if(score.get(sco) > max || (score.get(sco) == max && sco > maxScore)){
+                	max = score.get(sco);
+                    maxScore = sco;
                 }
             }
-            
-            System.out.println("#" + test_case + " " + maxidx);
-            
+			
+            bw.write("#" + test_case + " " + maxScore + "\n");
 		}
+        br.close();
+        bw.flush();
+        bw.close();
+        
 	}
 }
